127459d39f549cef76456bdbbd563b1f63146e62
Jeevan <javaindustryexperience@gmail.com>
2024-01-24 21:26:13 +0530

    added arraylist code for city

---
+++ GITDemo/src/com/velocity/ArrayListDemo1.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("pune");
+		al.add("mumbai");
+		al.add("bangalore");
+		al.add("delhi");
+		System.out.println(al);
+	}
+}
